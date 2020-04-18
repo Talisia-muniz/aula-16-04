c31eee882a0471b3b55ebf1de004ac3cdb3bbae3
Talisia-muniz <taliscorretora@hotmail.com>
2020-04-18 11:01:51 -0300

    exercícios

---
+++ js.javascript.js
@@ -0,0 +1,22 @@
+
+let valor1;
+let valor2;
+let total;
+
+function capturarvalor1(x) { 
+    valor1 = parseInt(x)
+}
+function capturarvalor2(y){
+     valor2 = parseInt(y);
+}     
+  function somar(){
+      total = valor1 + valor2;
+      alert(`o total da soma é ${total}`);
+}
+function subtrair(){
+    total = valor1 - valor2;
+    alert(`o total da soama é ${total}`);
+}
+
+
+  
